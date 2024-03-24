49bf447709f6ca4a4790eaca2100e96e97a42768
Leonablu <sergey.garbuzov.97@gmail.com>
2024-03-24 16:03:56 +0300

    Ready

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,8 @@ function addCard(cardItem, deleteCallback) {
   const cardElement = cardTemplate
     .querySelector(".places__item").cloneNode(true);
 
-  let imageCard = cardElement.querySelector(".card__image");
-  let titleCard = cardElement.querySelector(".card__title");
+  const imageCard = cardElement.querySelector(".card__image");
+  const titleCard = cardElement.querySelector(".card__title");
   const deleteButtonCard = cardElement.querySelector(".card__delete-button");
   const likeButtonCard = cardElement.querySelector(".card__like-button");
 
@@ -16,13 +16,9 @@ function addCard(cardItem, deleteCallback) {
   imageCard.alt = cardItem.name;
   titleCard.textContent = cardItem.name;
   // @todo: Функция удаления карточки
-  deleteButtonCard.addEventListener("click", function () {
-    if (typeof deleteCallback === "function") {
-      deleteCallback(cardElement);
-    } else {
-      cardElement.remove();
-    }
-  });
+  deleteButtonCard.addEventListener('click', function () {
+    deleteCallback(cardElement)
+  }); 
 
   likeButtonCard.addEventListener("click", function (evt) {
     evt.target.classList.toggle("card__like-button");
@@ -30,7 +26,12 @@ function addCard(cardItem, deleteCallback) {
 
   return cardElement;
 }
+
+function handleDelete (cardElement) {
+  cardElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function (cardItem, deleteCallback) {
-  placesList.append(addCard(cardItem, deleteCallback));
+initialCards.forEach(function (cardItem) {
+  placesList.append(addCard(cardItem, handleDelete));
 });
