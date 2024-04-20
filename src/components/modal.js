4482b752bd559b0f88b0d583bd8434d88cd31e40
Leonablu <sergey.garbuzov.97@gmail.com>
2024-04-20 21:19:44 +0300

    Ready

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,7 +1,7 @@
 // Функция закрытия модального окна через Escape
 function handleEscClose(event) {
   if (event.key === 'Escape') {
-    const openedModal = document.querySelector('.popup_open');
+    const openedModal = document.querySelector('.popup_is-opened');
     if (openedModal) {
       closeModal(openedModal);
     }
@@ -9,14 +9,14 @@ function handleEscClose(event) {
 }
 // Функция закрытия модального окна через клик по оверлею
 function onModalOverlayClick(event) {
-  if (event.target.classList.contains('popup_open')) {
+  if (event.target.classList.contains('popup_is-opened')) {
     closeModal(event.target);
   }
 }
 // Функция открытия модального окна 
 function openModal(modalElement) {
   if (modalElement != null) {
-    modalElement.classList.add('popup_open');
+    modalElement.classList.add('popup_is-opened');
     document.addEventListener('keydown', handleEscClose);
     modalElement.addEventListener('mousedown', onModalOverlayClick);
   }
@@ -24,7 +24,7 @@ function openModal(modalElement) {
 // Функция закрытия модального окна 
 function closeModal(modalElement) {
   if (modalElement != null) {
-    modalElement.classList.remove('popup_open');
+    modalElement.classList.remove('popup_is-opened');
     document.removeEventListener('keydown', handleEscClose);
     modalElement.removeEventListener('mousedown', onModalOverlayClick);
   }
