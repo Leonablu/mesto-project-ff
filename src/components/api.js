b0bf8abb96928ff3c5f8dc528edecda20c2b724f
Leonablu <sergey.garbuzov.97@gmail.com>
2024-05-14 23:54:32 +0300

    ready

--- src/components/api.js
+++ src/components/api.js
@@ -1,10 +1,10 @@
 const config = {
-  baseUrl: 'https://nomoreparties.co/v1/wff-cohort-13',
+  baseUrl: "https://nomoreparties.co/v1/wff-cohort-13",
   headers: {
-    authorization: '7850dbf9-efda-43c4-892a-b453102e2ca3',
-    'Content-Type': 'application/json'
-  }
-}
+    authorization: "7850dbf9-efda-43c4-892a-b453102e2ca3",
+    "Content-Type": "application/json",
+  },
+};
 const checkResponse = (res) => {
   if (res.ok) {
     return res.json();
@@ -14,95 +14,93 @@ const checkResponse = (res) => {
 // Функция для просмотра информации о пользователе
 export const getUserInfo = () => {
   return fetch(`${config.baseUrl}/users/me`, {
-    headers: config.headers
-  })
-.then(checkResponse)
-    .then((data) => {
-      console.log(data);
-      return data
-    });
-}
-// Функция для посмотра массива с карточками
-export const getInitialCards = () => {
-  return fetch(`${config.baseUrl}/cards`, {
-    headers: config.headers
+    headers: config.headers,
   })
     .then(checkResponse)
     .then((data) => {
       console.log(data);
-      return data
+      return data;
     });
-} 
+};
+// Функция для посмотра массива с карточками
+export const getInitialCards = () => {
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: config.headers,
+  })
+    .then(checkResponse)
+    .then((data) => {
+      console.log(data);
+      return data;
+    });
+};
 // Функция для обновлении информации о пользователе
 export const updateUserInfo = (userData) => {
   return fetch(`${config.baseUrl}/users/me`, {
-    method: 'PATCH',
+    method: "PATCH",
     headers: config.headers,
     body: JSON.stringify({
       name: userData.name,
       about: userData.about,
-      avatar: userData.avatar
+      avatar: userData.avatar,
+    }),
+  })
+    .then(checkResponse)
+    .then((data) => {
+      console.log(data);
+      if (userData.avatar && document.querySelector(".profile__image")) {
+        document.querySelector(
+          ".profile__image"
+        ).style.backgroundImage = `url('${userData.avatar}')`;
+      }
+      return data;
     })
-  })
-  .then(checkResponse)
-  .then((data) => {
-    console.log(data);
-    if (userData.avatar && document.querySelector('.profile__image')) {
-      document.querySelector('.profile__image').style.backgroundImage = `url('${userData.avatar}')`;
-    }
-    return data;
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
 // Функция для добавления новой карточки на сервер
 export const addNewCard = (cardData) => {
   return fetch(`${config.baseUrl}/cards`, {
-    method: 'POST',
+    method: "POST",
     headers: config.headers,
     body: JSON.stringify({
       name: cardData.name,
-      link: cardData.link
-    })
-  })
-  .then(checkResponse)
-}
+      link: cardData.link,
+    }),
+  }).then(checkResponse);
+};
 // Функция для добавления лайка
 export const likeCard = (cardId) => {
   return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-    method: 'PUT',
-    headers: config.headers
-  })
-  .then(checkResponse)
-}
+    method: "PUT",
+    headers: config.headers,
+  }).then(checkResponse);
+};
 // Функция для удаления лайка
 export const dislikeCard = (cardId) => {
   return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-    method: 'DELETE',
-    headers: config.headers
-  })
-  .then(checkResponse)
-}
+    method: "DELETE",
+    headers: config.headers,
+  }).then(checkResponse);
+};
 // Функция для удаления карточки
 export const removeCard = (cardId) => {
   return fetch(`${config.baseUrl}/cards/${cardId}`, {
-    method: 'DELETE',
-    headers: config.headers
-  })
-  .then(checkResponse)
-}
+    method: "DELETE",
+    headers: config.headers,
+  }).then(checkResponse);
+};
 // Функция для добавления аватара
 export const addNewAvatar = (avatarUrl) => {
   return fetch(`${config.baseUrl}/users/me/avatar`, {
-    method: 'PATCH',
+    method: "PATCH",
     headers: config.headers,
     body: JSON.stringify({
       avatar: avatarUrl,
-    })
+    }),
   })
-  .then(checkResponse)
-  .then(data => {
-    console.log('Изображение успешно добавлено', data);
-  });
-}
+    .then(checkResponse)
+    .then((data) => {
+      console.log("Изображение успешно добавлено", data);
+    });
+};
